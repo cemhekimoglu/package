6089040353cdc1edab2be73b69dc1b9bad1f477e
Cem <cemhekimoglu.ch@gmail.com>
2018-04-13 16:58:41 +0000

    made changes to the assignment

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var chalk = require("chalk");
 
 var message = chalk.redBright("Hello ") + chalk.bgCyan("World");
+var messageTwo = chalk.bold.blue("Cem ") + chalk.greenBright("Hekimoglu")
 console.log(message);
+console.log(messageTwo);
